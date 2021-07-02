1a6b0e5e883561d082c7c091be834fb14b1dc66c
Charles Lien <charleslien97@gmail.com>
2021-07-02 14:31:54 -0700

    Change bout state class to immutable

--- action.py
+++ action.py
@@ -29,3 +29,59 @@ class Action:
               ','.join(str(c.value) for c in self.played_cards) +
               ')')
     return f'({self.player},{self.main_action.value})'
+
+def getAttackingActions(active_player, hand, claimable):
+  all_actions = Action(active_player, MainAction.PASS)
+
+  for c in card.getCardsByType(card.ActionType.ATTACK):
+    max_claim_num = min(
+        sum(hand.values()),
+        claimable.get(c, 0)
+    )
+    for num_cards_to_claim in range(1, max_claim_num):
+      for cards_to_claim in utils.getAllUniqueCombinationsOfCards(
+          hand, num_cards_to_claim
+      ):
+        all_actions.append(Action(
+            active_player,
+            MainAction.CLAIM,
+            num_cards_to_claim,
+            c,
+            cards_to_claim
+        ))
+
+  return all_actions
+
+def getDefendingActions(active_player, hand, attack, claimable):
+  all_actions = [
+      Action(active_player, MainAction.PASS),
+      Action(active_player, MainAction.CHALLENGE)
+  ]
+
+  for c in card.getCardsByType(
+      card.ActionType.DEFEND,
+      damage_type=card.getDamageType(attack.card)
+  ):
+    max_claim_num = min(
+        sum(hand.values()),
+        claimable.get(c, 0),
+        attack.count
+    )
+    for num_cards_to_claim in range(1, max_claim_num + 1):
+      for cards_to_claim in utils.getAllUniqueCombinationsOfCards(
+          hand, num_cards_to_claim):
+        all_actions.append(Action(
+            active_player,
+            MainAction.CLAIM,
+            count=num_cards_to_claim,
+            card=c,
+            played_cards=cards_to_claim
+        ))
+
+  return all_actions
+
+def getLastActions(active_player):
+  return [
+      Action(active_player, MainAction.PASS),
+      Action(active_player, MainAction.CHALLENGE)
+  ]
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import enum
+import random
 
 class StrValueEnum(enum.IntEnum):
   def __str__(self):
@@ -18,4 +19,43 @@ def listToCountDict(lst):
   for val in lst:
     ret[val] = ret.get(val, 0) + 1
   
+  return ret
+
+def shuffleList(lst):
+  length = len(lst)
+  for i in range(length):
+    j = int(random.random() * length)
+    tmp = lst[i]
+    lst[i] = lst[j]
+    lst[j] = tmp
+
+def drawCards(deck, num_cards):
+  cards_to_draw = min(num_cards, len(deck))
+
+  return listToCountDict(deck[-cards_to_draw:]), deck[:-cards_to_draw]
+
+def getAllUniqueCombinationsOfCards(count_dict, num_cards):
+  ret = []
+
+  stack = [(card.Card(0), {}, num_cards)]
+  while stack:
+    c, curr, num_left = stack.pop()
+
+    max_num_of_c = min(count_dict.get(c, 0), num_left)
+    if count_dict.get(c, 0) >= num_left:
+      temp_dict = curr.copy()
+      temp_dict[c] = num_left
+      red.append(frozendict(temp_dict))
+      max_num_of_c -= 1
+
+    try:
+      next_card = card.Card(c.value + 1)
+    except ValueError as e:
+      continue
+
+    for num_of_c in range(max_num_of_c + 1):
+      temp_dict = curr.copy()
+      temp_dict[c] = num_of_c
+      stack.append((next_card, temp_dict, num_left - num_of_c))
+
   return ret
